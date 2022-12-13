400398bdcd834f5559adb1e9e5baad6831ddeadf
TessierV <5628@holbertonstudents.com>
2022-12-13 03:01:02 -0800

    forgot to take out test fucntion

--- monty.h
+++ monty.h
@@ -36,11 +36,7 @@ typedef struct instruction_s
 	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
-extern int ARG;
+/*extern int ARG;*/
 int main(int argc, char **argv);
 instruction_t *get_function(char *line);
-
-int is_int(char *str);
-void push(stack_t **stack, unsigned int line_number);
-void pall(stack_t **stack, unsigned int line_number);
 #endif
