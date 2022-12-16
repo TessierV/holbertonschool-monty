0d7049928c47d52e019df237049664386315945f
kiki3165 <kikikou82@gmail.com>
2022-12-16 16:48:50 +0000

    fixbetty

--- 3-opcodes.c
+++ 3-opcodes.c
@@ -15,6 +15,7 @@ void _swap(stack_t **stack, unsigned int line_number)
 	{
 		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
 		free_t(*stack);
+		
 		exit(EXIT_FAILURE);
 	}
 	swp = (*stack)->n;
@@ -22,6 +23,7 @@ void _swap(stack_t **stack, unsigned int line_number)
 	{
 		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
 		free_t(*stack);
+
 		exit(EXIT_FAILURE);
 	}
 	(*stack)->n = ((*stack)->next)->n;
